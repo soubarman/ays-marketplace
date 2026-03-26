c4b0d2e83d158476c7df9cedc3659fbfbf60eeaf
Sourav Barman <125635098+soubarman@users.noreply.github.com>
2026-03-26 14:19:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://ays-marketplace.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
